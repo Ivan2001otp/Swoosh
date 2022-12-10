dcb0345799e736922d5bd350a46ce7814b7044f2
Immanuel Dsouza <dsouzad681@gmail.com>
2022-12-10 16:28:25 +0530

    second commit 4:28pm dec 10-2022

---
+++ app/src/main/java/com/example/swoosh/ChoiceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChoiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choice)
+    }
+}
--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.swoosh.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+        binding.getStartedBtn.setOnClickListener(View.OnClickListener { v->
+            val firstIntent:Intent  = Intent(this,ChoiceActivity::class.java)
+            startActivity(firstIntent)
+            finish()
+        })
     }
 }
